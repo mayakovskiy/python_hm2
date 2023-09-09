2892cf1a6004f85b8d0ce2315660b759ba6f28fb
yauhen <greenandgreen8@gmail.com>
2023-09-10 00:35:47 +0300

    24

--- hm4.py
+++ hm4.py
@@ -1,4 +1,5 @@
 #задача 22
+"""
 n = int(input("Введите количество элементов в первом наборе: "))
 m = int(input("Введите количество элементов во втором наборе: "))
 
@@ -24,3 +25,25 @@ print("Уникальные элементы, встречающиеся в об
 for element in intersection:
     print(element)
 
+"""
+
+#задача 24
+N = int(input("Введите количество кустов: "))
+berries = []
+
+print("Введите количество ягод на каждом кусте:")
+for i in range(N):
+    berry_count = int(input())
+    berries.append(berry_count)
+
+
+collected_berries = []
+for i in range(N):
+    collected = berries[i] + berries[(i + 1) % N] + berries[(i + 2) % N]
+    collected_berries.append(collected)
+
+
+max_collected = max(collected_berries)
+max_position = collected_berries.index(max_collected) + 1 
+print("Максимальное количество собранных ягод:", max_collected)
+print("Позиция (номер куста), перед которой собрано максимальное количество ягод:", max_position)
